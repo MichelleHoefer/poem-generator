6c71856ba9f49e933f65cf2ff5bbf739b4e6658f
Michelle Hoefer <michellehoefer7@gmail.com>
2025-05-03 20:01:32 +0100

    adjusted css and added finishing touches

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Poem Generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -22,9 +21,6 @@ function generatePoem(event) {
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `⏳ Generating your poem about ${promptInput.value}...`;
 
-  console.log("Generating Poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
   axios.get(apiUrl).then(displayPoem);
 }
 
